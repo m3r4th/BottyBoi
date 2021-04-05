2eaec66642235ce2aed9b50ece5d1a0296c6a686
3r4th <3r4th@gmx.de>
2021-04-05 19:06:57 +0200

    Added basic fuctionality. (Ping, addQuote, etc.)

--- Main.py
+++ Main.py
@@ -1,2 +1,34 @@
-import os
+from dotenv import dotenv_values
+# Get Token from .env
+token = dotenv_values("token.env").get("DISCORD_TOKEN")
+#Set prefix and initalize
 import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='+')
+
+@bot.event
+async def on_ready():
+    print("Bot ready!")
+
+@bot.event
+async def on_member_koin(member):
+    print(member + " joined")
+
+@bot.event
+async def on_member_remove(member):
+    pass
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong! " + str(round(bot.latency * 1000)) + "ms")
+
+@bot.command()
+async def ding(ctx):
+    await ctx.send("Dong!")
+
+@bot.command(aliases=["addq"])
+async def addQ(ctx, author, quote):
+    await ctx.send("**\"" + quote + "\"" + " Author: " + author + "** was added!")
+
+bot.run(token)

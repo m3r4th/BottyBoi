a0c17fa0c0ee05569c5c206f50178e73b435b59e
3r4th <3r4th@gmx.de>
2022-06-03 20:54:23 +0200

    Added Guild-ID check Git-Ignore .txt files

--- Main.py
+++ Main.py
@@ -1,34 +1,45 @@
 from dotenv import dotenv_values
-# Get Token from .env
-token = dotenv_values("token.env").get("DISCORD_TOKEN")
-#Set prefix and initalize
-import discord
+# import discord
 from discord.ext import commands
 
+# Set prefix and initialize
 bot = commands.Bot(command_prefix='+')
 
 @bot.event
 async def on_ready():
     print("Bot ready!")
 
+
 @bot.event
-async def on_member_koin(member):
+async def on_member_join(member):
     print(member + " joined")
+
 
 @bot.event
 async def on_member_remove(member):
     pass
 
+
 @bot.command()
 async def ping(ctx):
     await ctx.send("Pong! " + str(round(bot.latency * 1000)) + "ms")
+
 
 @bot.command()
 async def ding(ctx):
     await ctx.send("Dong!")
 
+@bot.command(aliases=["guild"])
+async def guild_id(ctx):
+    await ctx.send(f"Guild-ID is {ctx.guild.id}")
+
+
 @bot.command(aliases=["addq"])
 async def addQ(ctx, author, quote):
     await ctx.send("**\"" + quote + "\"" + " Author: " + author + "** was added!")
 
-bot.run(token)
+
+if __name__ == '__main__':
+    # Get token from .env
+    token = dotenv_values("token.env").get("DISCORD_TOKEN")
+    bot.run(token)

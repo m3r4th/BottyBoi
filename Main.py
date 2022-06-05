23939e019f7bf64f7f72bc1378e4b58521b8240f
3r4th <3r4th@gmx.de>
2022-06-05 14:17:00 +0200

    Fix bug

--- Main.py
+++ Main.py
@@ -50,7 +50,7 @@ async def add_q(ctx, author, quote):
             print(adder_rsp + "\n")
         return
     elif len(adder_rsp) == 1:
-        adder_id = adder_rsp[1]
+        adder_id = adder_rsp
     else:
         ctx.send("You are trying to add a comment without being signed-up on this server\n"
                  "Please use \"+signup\" first.")
@@ -66,7 +66,7 @@ async def add_q(ctx, author, quote):
             print(author_rsp + "\n")
         return
     elif len(author_rsp) == 1:
-        author_id = author_rsp[1]
+        author_id = author_rsp
     else:
         ctx.send("You are trying to add a quote to a user that does not exist.\n"
                  "Please check \"+users\" first.")
